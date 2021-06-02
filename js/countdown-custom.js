9fc022a90491b2a2e7ec4cfb0e19e26c42031a5a
Tanishq <tanishedu@gmail.com>
2021-06-02 22:19:27 +0530

    layout updated with all new theme

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2021, 6-1, 4, 17)}); // year, month, date, hour
+        });
+});		
+
